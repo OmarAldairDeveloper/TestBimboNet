640e95f6a67cfce7166585355bf8576821c955de
OmarAldairDeveloper <omaraldair.developer@gmail.com>
2023-05-29 13:41:26 -0600

    Initial Commit

---
+++ PruebaTecnicaBimbo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PruebaTecnicaBimbo
+//
+//  Created by Omar Aldair on 29/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
